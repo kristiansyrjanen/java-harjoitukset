0e28186a435d37dd78066944471a143bf36faaad
Kristian Syrjänen <35492474+kristiansyrjanen@users.noreply.github.com>
2019-01-29 11:54:35 +0200

    Update Harjoitus5.2.java

--- Harjoitus5.1-5/Harjoitus5.2.java
+++ Harjoitus5.1-5/Harjoitus5.2.java
@@ -8,21 +8,23 @@ public class Rekisterinumerot {
 		String[] rekisteriNumerot = new String[10]; // max 10
 		String rekkari = null;
 		int lkm = 0; // tämänhetkinen lkm
-		String exit = "-"; // "" Muista hipsut
+		String exit = "-"; // "" Muista hipsut stringin yhteydessä
 		System.out.print("Anna rekisterinumero (- lopettaa): ");
 
 		rekkari = input.nextLine();
+		rekkari = rekkari.toUpperCase();
 		while (lkm < 10 && !rekkari.equals(exit)) {
 			rekisteriNumerot[lkm] = rekkari;
 			lkm++;
 			System.out.print("Anna rekisterinumero (- lopettaa): ");
 			rekkari = input.nextLine();
+			rekkari = rekkari.toUpperCase();
 		}
 
 		Arrays.sort(rekisteriNumerot, 0, lkm);
 		System.out.println("Rekisterinumerot ovat: ");
 		for (int i = 0; i < lkm; i++) {
-			System.out.println(rekisteriNumerot[i].toUpperCase());
+			System.out.println(rekisteriNumerot[i]);
 		}
 
 	}
